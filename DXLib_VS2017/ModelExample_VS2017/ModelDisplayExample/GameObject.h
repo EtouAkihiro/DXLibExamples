71b64f9b2efa33eb52ed003a5a8bbdb7c4e76bfa
Eto <akimagi26@gmail.com>
2021-07-10 15:27:03 +0900

    GameObjectの土台を作成

--- DXLib_VS2017/ModelExample_VS2017/ModelDisplayExample/GameObject.h
+++ DXLib_VS2017/ModelExample_VS2017/ModelDisplayExample/GameObject.h
@@ -1,9 +1,14 @@
 #ifndef GAME_OBJECT_H_
 #define GAME_OBJECT_H_
 
+#include <string>
+
+#include "Vector3.h"
+
 // ゲームオブジェクトクラス（基底クラス）
 class GameObject
 {
+public:
 	// コンストラクタ
 	GameObject() = default;
 	// デストラクタ
@@ -13,6 +18,29 @@ class GameObject
 	virtual void Update();
 	// 描画
 	virtual void Draw();
+
+	// 死亡する
+	void Die();
+	// 死亡しているか？
+	bool IsDead() const;
+	// タグ名を取得
+	const std::string& Tag() const;
+	// 名前を取得
+	const std::string& Name() const;
+
+	// コピー禁止
+	GameObject(const GameObject& other) = delete;
+	GameObject& operator = (const GameObject& other) = delete;
+
+protected:
+	// タグ名
+	std::string m_Tag;
+	// 名前
+	std::string m_Name;
+	// 位置
+	Vector3 m_Position;
+	// 死亡フラグ
+	bool m_Dead{ false };
 };
 
 #endif // !GAME_OBJECT_H_
