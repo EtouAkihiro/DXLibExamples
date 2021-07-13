fe8185a10a53b71bb27fa523856c9bef63d5ce61
Eto <akimagi26@gmail.com>
2021-07-13 16:37:21 +0900

    トランスフォームに必要な関数と変数を追加

--- DXLib_VS2017/ModelExample_VS2017/ModelDisplayExample/Transform.h
+++ DXLib_VS2017/ModelExample_VS2017/ModelDisplayExample/Transform.h
@@ -23,26 +23,53 @@ public:
 	void SetPosition(VECTOR position);
 	// ワールド座標を取得
 	VECTOR GetPosition() const;
+
 	// ローカル座標を設定
 	void SetLocalPosition(VECTOR localPosition);
 	// ローカル座標を取得
 	VECTOR GetLocalPosition() const;
+
 	// ワールドの回転を設定
 	void SetRotation(VECTOR rotation);
 	// ワールドの回転を取得
 	VECTOR GetRotation() const;
+
 	// ローカルの回転を設定
 	void SetLocalRotation(VECTOR localRotation);
 	// ローカルの回転を取得
 	VECTOR GetLocalRotation() const;
+
 	// ワールドのスケール値を設定
 	void SetScale(VECTOR scale);
 	// ワールドのスケール値を取得
 	VECTOR GetScale() const;
+
 	// ローカルのスケール値を設定
 	void SetLocalScale(VECTOR localScale);
 	// ローカルのスケール値を取得
 	VECTOR GetLocalScale() const;
+
+	// 前向き方向を設定
+	void SetForward(VECTOR forward);
+	// 前向き方向を取得
+	VECTOR GetForward() const;
+
+	// 右向き方向を設定
+	void SetRight(VECTOR right);
+	// 右向き方向を取得
+	VECTOR GetRight() const;
+
+	// 上向き方向を設定
+	void SetUp(VECTOR up);
+	// 上向き方向を取得
+	VECTOR GetUp() const;
+
+	// オブジェクト方向を見る。
+	void LookAt(Transform target, VECTOR worldUp);
+	void LookAt(Transform target);
+	// 移動方向を見る。
+	void LookAt(VECTOR worldPosition, VECTOR worldUp);
+	void LookAt(VECTOR worldPosition);
 
 private:
 	// 座標(ワールド座標)
