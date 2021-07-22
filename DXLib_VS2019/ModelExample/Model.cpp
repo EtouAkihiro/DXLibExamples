e916205347b80d118d50800aabbf3556696e5b12
Eto_Akihiro0206 <akihiroeto1326@gmail.com>
2021-07-22 21:32:16 +0900

    Transform用の描画関数を作成

--- DXLib_VS2019/ModelExample/Model.cpp
+++ DXLib_VS2019/ModelExample/Model.cpp
@@ -1,5 +1,8 @@
 #include "Model.h"
+
 #include "DxLib.h"
+#include "DXTransform.h"
+
 
 // シンプルモデル
 int Model::SimpleModel{ -1 };
@@ -10,7 +13,7 @@ void Model::Load()
 	SimpleModel = MV1LoadModel("Models/Robot Kyle.mv1");
 }
 
-// 描画
+// 描画（Vector版）
 void Model::Draw(int model, VECTOR position, VECTOR rotation, VECTOR scale)
 {
 	// モデルの座標を設定
@@ -19,6 +22,19 @@ void Model::Draw(int model, VECTOR position, VECTOR rotation, VECTOR scale)
 	MV1SetRotationXYZ(model, rotation);
 	// モデルの大きさを設定
 	MV1SetScale(model, scale);
+	// 描画
+	MV1DrawModel(model);
+}
+
+// 描画（Transform版）
+void Model::Draw(int model, DXTransform transform)
+{
+	// モデルの座標を設定
+	MV1SetPosition(model, transform.Position());
+	// モデルの回転を設定
+	MV1SetRotationXYZ(model, transform.Rotation());
+	// モデルの大きさ
+	MV1SetScale(model, transform.Scale());
 	// 描画
 	MV1DrawModel(model);
 }
--- DXLib_VS2019/ModelExample/Model.h
+++ DXLib_VS2019/ModelExample/Model.h
@@ -3,14 +3,18 @@
 
 #include "DxLib.h"
 
+class DXTransform;
+
 // モデルの読み込みクラス
 class Model 
 {
 public:
 	// 読み込み
 	static void Load();
-	// 描画
+	// 描画（Vector版）
 	static void Draw(int model, VECTOR position, VECTOR rotation, VECTOR scale);
+	// 描画（Transform版）
+	static void Draw(int model, DXTransform transform);
 
 public:
 	// シンプルモデル
